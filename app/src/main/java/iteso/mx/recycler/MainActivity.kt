e52483d8005d3320fc4e9df0db4491cb95490f49
santiago <santiagosoule96@yahoo.com>
2019-10-14 19:22:40 -0500

    feeder modified

--- app/src/main/java/iteso/mx/recycler/MainActivity.kt
+++ app/src/main/java/iteso/mx/recycler/MainActivity.kt
@@ -12,6 +12,13 @@ import com.parse.ParseQuery
 import iteso.mx.recycler.adapters.AdapterName
 import org.jetbrains.anko.getStackTraceString
 import org.jetbrains.anko.uiThread
+import java.io.File
+import java.util.*
+
+private var names = arrayListOf<String>()
+private var lastNames = arrayListOf<String>()
+private var birthDate = arrayListOf<Date>()
+private var photos = arrayListOf<File>()
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,24 +26,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         val recyclerView = findViewById<RecyclerView>(R.id.activity_main_names_rv)
-        val names = arrayListOf<HashMap<String, String>>()
-        names.add(HashMap())
-        names[0].put("name", "Erick")
-        names[0].put("lastName", "De Santiago")
-        names.add(HashMap())
-        names[1].put("name", "Edmundo")
-        names[1].put("lastName", "Vidalvarez")
-        names.add(HashMap())
-        names[2].put("name", "Rogelio")
-        names[2].put("lastName", "Sandoval")
-        names.add(HashMap())
-        names[3].put("name", "Javier")
-        names[3].put("lastName", "Gil")
-        names.add(HashMap())
-        names[4].put("name", "Alberto")
-        names[4].put("lastName", "Miramontes")
+
         recyclerView.adapter = AdapterName(names)
         recyclerView.layoutManager = LinearLayoutManager(this)
     }
@@ -47,7 +38,12 @@ class feed {
         var query = ParseQuery<ParseObject>("UserList")
         query.findInBackground(FindCallback { user_list: List<ParseObject>, e ->
             if(e == null)
-                Log.d("user", "Retrieved " + user_list.size + " users")
+                for (user in user_list) {
+                    names.add(user.name)
+                    lastNames.add(user.lastName)
+                    birthDate.add(user.birthDate)
+                    photos.add(user.photo)
+                }
             else
                 error{e}
         })
